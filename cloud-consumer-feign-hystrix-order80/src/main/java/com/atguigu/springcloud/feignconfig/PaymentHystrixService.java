a7672ef7e747cf51c3c20208e665d9b8387c2ad4
tom_glb <tom_glb@atguigu.com>
2021-12-03 17:23:03 +0800

    fourth springcloud project

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/feignconfig/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/feignconfig/PaymentHystrixService.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
 @FeignClient(value = "cloud-provider-hystrix-payment",fallback = PaymentFallbackService.class)
-public interface PaymentHystrixService {
+public interface
+PaymentHystrixService {
 
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_OK(@PathVariable("id") Integer id);
